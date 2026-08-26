63114c8776449cd1a774b67494c91ea3b12eb5e6
HeryLiao <65938042+HeryLiao@users.noreply.github.com>
2026-08-26 20:43:00 +0800

    Add: my-calendar-i - submission-0

---
+++ Data Structures & Algorithms/my-calendar-i/submission-0.cpp
@@ -0,0 +1,27 @@
+class MyCalendar {
+private:
+    vector<pair<int , int>> books;
+
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int startTime, int endTime) {
+        for(auto b : books){
+            if(startTime < b.second && endTime > b.first){
+                return false;
+            }
+
+        }
+        books.push_back({startTime , endTime});
+        return true;
+    
+    }     
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */

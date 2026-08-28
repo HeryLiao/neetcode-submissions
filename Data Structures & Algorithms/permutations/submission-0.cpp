0f5ca7db9d6f1b1705f9275c1a1d2a19608083d3
HeryLiao <65938042+HeryLiao@users.noreply.github.com>
2026-08-29 00:13:09 +0800

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    vector<vector<int>> res;
+    vector<int>path;
+    void backtrack(vector<int>&nums, vector<bool>& used){
+        if(path.size() == nums.size()){
+            res.push_back(path);
+            return;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(used[i] == true){
+                continue;
+            }
+            path.push_back(nums[i]);
+            used[i] = true;
+            backtrack(nums ,used);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        res.clear();
+        path.clear();
+        vector<bool> used (nums.size(), false);
+        backtrack(nums, used);
+        return res;
+    }
+};
